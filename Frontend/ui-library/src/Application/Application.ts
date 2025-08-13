c8fb4a617999ddbdf1c80574c13a64a72ff189d1
Ken <ken@nustechnology.com>
2025-08-13 15:23:49 +0700

    show only message when subscribe fail

--- Frontend/ui-library/src/Application/Application.ts
+++ Frontend/ui-library/src/Application/Application.ts
@@ -798,7 +798,8 @@ export class Application {
     }
 
     handleSubscribeFailedMessage(message: string) {
-        this.showDisconnectOverlay(`Subscribe failed: "${message}" Click to try again`);
+        // this.showDisconnectOverlay(`Subscribe failed: "${message}" Click to try again`);
+        this.showDisconnectOverlay(message);
         document.cookie = 'SERVERID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     }
 
