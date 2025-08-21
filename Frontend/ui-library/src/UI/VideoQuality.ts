b1615c12e0aa39ce28e73c9d8cc2d972e20462f9
Ken <ken@nustechnology.com>
2025-08-21 16:57:19 +0700

    display resolution text

--- Frontend/ui-library/src/UI/VideoQuality.ts
+++ Frontend/ui-library/src/UI/VideoQuality.ts
@@ -14,11 +14,33 @@ export class VideoQuality {
     }
 
     public updateQualityText(text: string) {
-        this._rootElement.textContent = text;
+        this._rootElement.textContent = this.displayText(text);
         if (text.length > 0) {
             this._rootElement.style.display = 'block';
         } else {
             this._rootElement.style.display = 'none';
+        }
+    }
+
+    private displayText(text: string): string {
+        switch (text) {
+            case '2560x1440':
+            case '2560x1600':
+                return '2K';
+            case '3840x2160':
+                return '4K';
+            case '7680x4320':
+                return '8K';
+            case '1920x1080':
+                return '1080p';
+            case '1280x720':
+                return '720p';
+            case '800x600':
+                return '600p';
+            case '640x480':
+                return '480p';
+            default:
+                return text;
         }
     }
 }
